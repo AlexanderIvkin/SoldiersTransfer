778a6afcc9d461e80f5e49115af041df25ae72d1
AlexanderIvkin <dvakota@bk.ru>
2024-12-12 16:17:19 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoldiersTransfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPlatoonCount = 20;
+            int secondPlatoonCount = 5;
+            List<string> possibleNames = new List<string>
+            {
+                "Богдан", "Борис", "Баян",
+                "Бронислав", "Бонифатий", "Богумил",
+                "Саня", "Лёха", "Максимыч",
+                "Михал", "Костян", "Спиридон"
+            };
+            SoldiersFactory soldiersFactory = new SoldiersFactory(possibleNames);
+            Transfer transfer = new Transfer(soldiersFactory.Create(firstPlatoonCount), soldiersFactory.Create(secondPlatoonCount));
+
+            transfer.Execute();
+        }
+    }
+
+    class Transfer
+    {
+        private List<Soldier> _platoon1;
+        private List<Soldier> _platoon2;
+
+        public Transfer(List<Soldier> platoon1, List<Soldier> platoon2)
+        {
+            _platoon1 = platoon1;
+            _platoon2 = platoon2;
+        }
+
+        public void Execute()
+        {
+            string firstPlatoonName = "Взвод 1";
+            string secondPlatoonName = "Взвод 2";
+            string requiredLiteral = "Б";
+
+            Console.WriteLine("BEFORE");
+            ShowInfo(_platoon1, firstPlatoonName);
+            ShowInfo(_platoon2, secondPlatoonName);
+
+            var transferedSoldiers = _platoon1.Where(soldier => soldier.Name.StartsWith(requiredLiteral));
+
+            _platoon1 = _platoon1.Except(transferedSoldiers).ToList();
+            _platoon2 = _platoon2.Union(transferedSoldiers).ToList();
+
+            Console.WriteLine("AFTER");
+            ShowInfo(_platoon1, firstPlatoonName);
+            ShowInfo(_platoon2, secondPlatoonName);
+        }
+
+        private void ShowInfo(List<Soldier> platoon, string platoonName)
+        {
+            int count = 1;
+
+            Console.WriteLine(platoonName);
+
+            foreach (Soldier soldier in platoon)
+            {
+                Console.WriteLine($"{count++} Имя - {soldier.Name}.");
+                
+            }
+        }
+    }
+
+    class SoldiersFactory
+    {
+        private List<string> _names;
+
+        public SoldiersFactory(List<string> names)
+        {
+            _names = names;
+        }
+
+        public List<Soldier> Create(int count)
+        {
+            List<Soldier> soldiers = new List<Soldier>();
+
+            for (int i = 0; i < count; i++)
+            {
+                soldiers.Add(new Soldier(_names[UserUtills.GenerateRandomPosiriveLimitedNumber(_names.Count)]));
+            }
+
+            return soldiers;
+        }
+    }
+
+    class Soldier
+    {
+        public string Name { get; }
+
+        public Soldier(string name)
+        {
+            Name = name;
+        }
+    }
+
+    static class UserUtills
+    {
+        private static Random s_random = new Random();
+
+        public static int GenerateRandomPosiriveLimitedNumber(int maxValueExclusive)
+        {
+            return s_random.Next(maxValueExclusive);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("SoldiersTransfer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoldiersTransfer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d86f610b-b710-44ff-9dd7-e4a0783d29c8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
